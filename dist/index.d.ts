60d551c43b9d51708b1349d319e7bc594154df06
zqylzcwcxy <347891134@qq.com>
2023-05-08 10:39:29 +0800

    :sparkles:feat: .gitignore

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function plus(a: number, b: number): number;
-export declare function minus(a: number, b: number): number;
-export declare function multiply(a: number, b: number): number;
-export declare function divide(a: number, b: number): number;
